9b2f49aeba66d0c5876be87fa287cc9b6f92e803
Saif-ur-Rehman Awan <saifurrehmanawan85@gmail.com>
2025-02-01 22:47:37 +0500

    initail menu designing just the box, not selection option currently available yet

--- My first game/game.cpp
+++ My first game/game.cpp
@@ -39,85 +39,104 @@ aabb_vs_aabb(float p1x, float p1y, float hs1x, float hs1y,
 		p1y - hs1y < p2y + hs2y);
 }
 
+enum Gamemode {
+	GM_MENU,
+	GM_GAMEPLAY,
+};
+
+Gamemode current_gamemode;
+int hot_button;
+
 internal void
 stimulate_game(Input* input, float dt) {
 
 	clear_screen(0xff5500); // orange backgound in entire window 
 	draw_rect(0, 0, arena_half_size_x, arena_half_size_y, 0xffaa33); // yellow background for gameplay
 
-	//comtrols moment for player 1
-	float player_1_dpp = 0.f;
+	if (current_gamemode == GM_GAMEPLAY) {
+		//comtrols moment for player 1
+		float player_1_dpp = 0.f;
 #if 0
-	if (is_down(BUTTON_UP)) player_1_dpp += 2000;
-	if (is_down(BUTTON_DOWN)) player_1_dpp -= 2000;
+		if (is_down(BUTTON_UP)) player_1_dpp += 2000;
+		if (is_down(BUTTON_DOWN)) player_1_dpp -= 2000;
 #else
-	//if (ball_p_y > player_1_p + 2.5f) player_1_dpp += 1300;
-	//if (ball_p_y < player_1_p - 2.5f) player_1_dpp -= 1300;
-	player_1_dpp = (ball_p_y - player_1_p) * 100;
-	if (player_1_dpp > 1300) player_1_dpp = 1300;
-	if (player_1_dpp < -1300) player_1_dpp = -1300;
+		//if (ball_p_y > player_1_p + 2.5f) player_1_dpp += 1300;
+		//if (ball_p_y < player_1_p - 2.5f) player_1_dpp -= 1300;
+		player_1_dpp = (ball_p_y - player_1_p) * 100;
+		if (player_1_dpp > 1300) player_1_dpp = 1300;
+		if (player_1_dpp < -1300) player_1_dpp = -1300;
 #endif
-	// controls moment for player 2
-	float player_2_dpp = 0.f;
-	if (is_down(BUTTON_W)) player_2_dpp += 2000;
-	if (is_down(BUTTON_S)) player_2_dpp -= 2000;
+		// controls moment for player 2
+		float player_2_dpp = 0.f;
+		if (is_down(BUTTON_W)) player_2_dpp += 2000;
+		if (is_down(BUTTON_S)) player_2_dpp -= 2000;
 
-	stimulate_player(&player_1_p, &player_1_dp, player_1_dpp, dt);
-	stimulate_player(&player_2_p, &player_2_dp, player_2_dpp, dt);
+		stimulate_player(&player_1_p, &player_1_dp, player_1_dpp, dt);
+		stimulate_player(&player_2_p, &player_2_dp, player_2_dpp, dt);
 
-	// stimulate ball
-	{
-		// code for ball moverment
-		ball_p_x += ball_dp_x * dt;
-		ball_p_y += ball_dp_y * dt;
+		// stimulate ball
+		{
+			// code for ball moverment
+			ball_p_x += ball_dp_x * dt;
+			ball_p_y += ball_dp_y * dt;
 
 
-		//ball collision 
-		if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) {
-			ball_p_x = 80 - player_half_size_x - ball_half_size;
-			ball_dp_x *= -1;
-			ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * 0.75f; // changing ball direction after collosion 
+			//ball collision 
+			if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 80, player_1_p, player_half_size_x, player_half_size_y)) {
+				ball_p_x = 80 - player_half_size_x - ball_half_size;
+				ball_dp_x *= -1;
+				ball_dp_y = (ball_p_y - player_1_p) * 2 + player_1_dp * 0.75f; // changing ball direction after collosion 
+			}
+			else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) {
+				ball_p_x = -80 + player_half_size_x + ball_half_size;
+				ball_dp_x *= -1;
+				ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * 0.75f;
+			}
+
+			// limiting ball to stay inside from vertical (up and down) collosion after player hit them
+			if (ball_p_y + ball_half_size > arena_half_size_y) {
+				ball_p_y = arena_half_size_y - ball_half_size;
+				ball_dp_y *= -1;
+			}
+			else if (ball_p_y - ball_half_size < -arena_half_size_y) {
+				ball_p_y = -arena_half_size_y + ball_half_size;
+				ball_dp_y *= -1;
+			}
+
+			// bringing ball back to live after each game over
+			if (ball_p_x + ball_half_size > arena_half_size_x) {
+				ball_dp_x *= -1;
+				ball_dp_y = 0;
+				ball_p_x = 0;
+				ball_p_y = 0;
+				player_1_score++;
+			}
+			else if (ball_p_x - ball_half_size < -arena_half_size_x) {
+				ball_dp_x *= -1;
+				ball_dp_y = 0;
+				ball_p_x = 0;
+				ball_p_y = 0;
+				player_2_score++;
+			}
 		}
-		else if (aabb_vs_aabb(ball_p_x, ball_p_y, ball_half_size, ball_half_size, -80, player_2_p, player_half_size_x, player_half_size_y)) {
-			ball_p_x = -80 + player_half_size_x + ball_half_size;
-			ball_dp_x *= -1;
-			ball_dp_y = (ball_p_y - player_2_p) * 2 + player_2_dp * 0.75f;
-		}
 
-		// limiting ball to stay inside from vertical (up and down) collosion after player hit them
-		if (ball_p_y + ball_half_size > arena_half_size_y) {
-			ball_p_y = arena_half_size_y - ball_half_size;
-			ball_dp_y *= -1;
-		}
-		else if (ball_p_y - ball_half_size < -arena_half_size_y) {
-			ball_p_y = -arena_half_size_y + ball_half_size;
-			ball_dp_y *= -1;
-		}
+		// showing player score by box on both sides top
+		draw_number(player_1_score, -10, 40, 1.f, 0xbbffbb);
+		draw_number(player_2_score, 10, 40, 1.f, 0xbbffbb);
 
-		// bringing ball back to live after each game over
-		if (ball_p_x + ball_half_size > arena_half_size_x) {
-			ball_dp_x *= -1;
-			ball_dp_y = 0;
-			ball_p_x = 0;
-			ball_p_y = 0;
-			player_1_score++;
+		//renering
+		draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // ball graphic
+		draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000); // player 1 graphic
+		draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000); // player 2 graphics
+	}
+	else {
+		if (hot_button == 0) {
+			draw_rect(20, 0, 10, 10, 0xff0000);
+			draw_rect(-20, 0, 10, 10, 0xcccccc);
 		}
-		else if (ball_p_x - ball_half_size < -arena_half_size_x) {
-			ball_dp_x *= -1;
-			ball_dp_y = 0;
-			ball_p_x = 0;
-			ball_p_y = 0;
-			player_2_score++;
+		else {
+			draw_rect(20, 0, 10, 10, 0xcccccc);
+			draw_rect(-20, 0, 10, 10, 0xff0000);
 		}
 	}
-
-	// showing player score by box on both sides top
-	draw_number(player_1_score, -10, 40, 1.f, 0xbbffbb);
-	draw_number(player_2_score, 10, 40, 1.f, 0xbbffbb);
-
-	//renering
-	draw_rect(ball_p_x, ball_p_y, ball_half_size, ball_half_size, 0xffffff); // ball graphic
-	draw_rect(80, player_1_p, player_half_size_x, player_half_size_y, 0xff0000); // player 1 graphic
-	draw_rect(-80, player_2_p, player_half_size_x, player_half_size_y, 0xff0000); // player 2 graphics
-
 }
